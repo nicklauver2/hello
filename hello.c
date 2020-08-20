c7616dbfddce42708cccd605244ae660903934a5
nicklauver2 <69817821+nicklauver2@users.noreply.github.com>
2020-08-20 11:32:54 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Nick Lauver
+ *         nlauver2@huskers.unl.edu
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nick Lauver!\n");
+
+  return 0;
+}
